26576acabf79bf606216cef932ac7d28bd32a782
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-09-11 23:32:23 +0530

    Create Oddnum.java

---
+++ Day39/Oddnum.java
@@ -0,0 +1,12 @@
+package Pranjal.day39;
+
+public class Oddnum {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i+"\n");
+            }
+        }
+    }
+}
+

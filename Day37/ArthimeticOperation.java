25488a7d343e027feda5b48740b21ceb135f21be
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-09-11 23:21:02 +0530

    Create ArthimeticOperation.java

---
+++ Day37/ArthimeticOperation.java
@@ -0,0 +1,14 @@
+public class ArthimeticOperation {
+
+        public static void main(String[] args) {
+            int a = 15;
+            int b = 4;
+
+            System.out.println("a + b = " + (a + b));
+            System.out.println("a - b = " + (a - b));
+            System.out.println("a * b = " + (a * b));
+            System.out.println("a / b = " + (a / b));
+            System.out.println("a % b = " + (a % b));
+        }
+    }
+

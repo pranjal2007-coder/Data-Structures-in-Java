1cabe0464cf92636dd7d7c197e9419afdefbadd7
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-09-11 23:26:00 +0530

    Create  EvenOdd.java

---
+++ Day38/EvenOdd.java
@@ -0,0 +1,16 @@
+
+package Pranjal.day38;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
+
+

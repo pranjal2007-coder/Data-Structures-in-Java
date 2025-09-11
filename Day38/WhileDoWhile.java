7654880d45682fc77bc7aec87bf08960ac8642a5
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-09-11 23:27:10 +0530

    Create WhileDoWhile.java

---
+++ Day38/WhileDoWhile.java
@@ -0,0 +1,20 @@
+package Pranjal.day38;
+
+public class WhileDoWhile {
+
+        public static void main(String[] args) {
+            int x = 5;
+            System.out.println("While");
+
+            while (x < 5) {
+                System.out.println("ok");
+            }
+
+            System.out.println("Do-while loop:");
+            do {
+                System.out.println("Hello");
+            } while (x < 5);
+        }
+    }
+
+

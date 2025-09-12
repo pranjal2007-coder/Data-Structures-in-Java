8237a85859462d51fbbe5e5e479cd3acbed26b7f
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-09-12 10:50:04 +0530

    Update HashSquarePattern.java

--- Day40/HashSquarePattern.java
+++ Day40/HashSquarePattern.java
@@ -2,11 +2,11 @@ package com.Pranjal.Day40;
 
 public class HashSquarePattern {
     public static void main(String [] args){
-        for(int i=1;i<=5;i++)
+        for(int i=1;i<=4;i++)
         {
-            for(int j=1;j<=5;j++)
+            for(int j=1;j<=4;j++)
             {
-                System.out.print(j+" ");
+                System.out.print("#");
             }
       System.out.println();  }
     }

a4e587e65d0baf7bfdef887d536e95e0c1aa16b2
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-09-12 10:59:24 +0530

    Update NestedLoopPattern.java

--- Day40/NestedLoopPattern.java
+++ Day40/NestedLoopPattern.java
@@ -1,18 +1,20 @@
 
 package com.Pranjal.Day40;
-
 public class NestedLoopPattern {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
+        int n = 7; 
+
+       
+        for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(j + " ");
             }
             System.out.println();
         }
 
-        for (int i = 4; i >= 1; i--) {
+        for (int i = 2; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(j + " ");
             }
             System.out.println();
         }

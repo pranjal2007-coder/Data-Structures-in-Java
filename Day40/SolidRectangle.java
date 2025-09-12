2f978da040d0b7a5bc2e02f65b644faafd31df16
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-09-12 10:24:00 +0530

    Create SolidRectangle.java

---
+++ Day40/SolidRectangle.java
@@ -0,0 +1,14 @@
+package com.Pranjal.Day40;
+
+public class SolidRectangle {
+    public static void main(String args []) {
+        for (int i = 1; i <= 4;i++ )
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print("*");
+            }
+      System.out.println();
+        }
+    }
+}

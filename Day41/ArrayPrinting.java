805e26af6d7cbbaad9d8fb03329b5b6a5e62c272
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-09-15 21:47:34 +0530

    Create ArrayPrinting.java

---
+++ Day41/ArrayPrinting.java
@@ -0,0 +1,11 @@
+package com.Pranjal.Day41;
+
+public class ArrayPrinting {
+    public static void main(String[] args)
+    {
+        int[] arr={2,3,4,5,6};
+        for(int i=0;i<5;i++)
+            System.out.println(arr[i]);
+
+    }
+}

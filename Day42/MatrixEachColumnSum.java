d15a83a159da3a000173626f39ef5502db5cfc35
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-09-16 10:48:13 +0530

    Create MatrixEachColumnSum.java

---
+++ Day42/MatrixEachColumnSum.java
@@ -0,0 +1,36 @@
+package com.Pranjal.Day42;
+
+import java.util.Scanner;
+
+public class MatrixEachColumnSum {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        int row,col,i,j;
+        System.out.println("Enter the number of rows: ");
+        row=sc.nextInt();
+        System.out.println("Enter the number of columns: ");
+        col=sc.nextInt();
+
+        int[][] mat=new int[row][col];
+
+        for(i=0; i< row; i++)
+        {
+            for(j=0;j<col;j++)
+            {
+                mat[i][j]=sc.nextInt();
+            }
+        }
+
+        for(j=0;j<col;j++)
+        {
+            int sum=0;
+            for(i=0; i< row; i++)
+            {
+               sum+=mat[i][j];
+            }
+            System.out.println("Sum is "+sum);
+        }
+    }
+}

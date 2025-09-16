64e2c6054b6aeac01b6ef460cd2219367ea42ca5
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-09-16 10:48:55 +0530

    Create RowsPrinting2dArray.java

---
+++ Day42/RowsPrinting2dArray.java
@@ -0,0 +1,41 @@
+package com.Pranjal.Day42;
+
+import java.util.Scanner;
+
+public class RowsPrinting2dArray {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int row,col,i,j;
+        System.out.println("Enter the number of rows: ");
+        row=sc.nextInt();
+        System.out.println("Enter the number of columns: ");
+        col=sc.nextInt();
+
+        int[][] mat=new int[row][col];
+
+        System.out.println("Enter the Elements of Matrix: ");
+        for(i=0; i< row; i++)
+        {
+            for(j=0;j<col;j++)
+            {
+                mat[i][j]=sc.nextInt();
+            }
+        }
+
+        for(i=0; i< row; i++)
+        {
+            if(i == 0||i==row-1)
+             for(j=0;j<col;j++)
+             {
+                 System.out.print(mat[i][j]);
+             }
+            System.out.println();
+        }
+
+
+            }
+        }
+
+
+

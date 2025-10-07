b7520ef496cc38e5c1c55d3fbcd03933f5fbf1db
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-10-07 12:27:04 +0530

    Add files via upload

---
+++ Day50/StringBufferExample.java
@@ -0,0 +1,13 @@
+package com.Pranjal.Day50;
+
+public class StringBufferExample {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Learning");
+
+        sb.append(" Java");
+        sb.append(" with fun");
+
+        System.out.println("Final Sentence: " + sb);
+    }
+}
+
---
+++ Day50/UppercaseConversion.java
@@ -0,0 +1,15 @@
+package com.Pranjal.Day50;
+
+public class UppercaseConversion {
+
+        public static void main(String[] args) {
+            String name = "pranjal";
+
+            String upper = name.toUpperCase();
+            System.out.println("Uppercase: " + upper);
+
+            char ch = upper.charAt(2);
+            System.out.println("Character at index 2: " + ch);
+        }
+    }
+

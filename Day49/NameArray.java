1402f30d3ecb44585b26a1d187c00587920ce0dc
pranjal2007-coder <pranjal0659.becse24@chitkara.edu.in>
2025-10-07 12:25:10 +0530

    Create NameArray.java

---
+++ Day49/NameArray.java
@@ -0,0 +1,13 @@
+package com.Pranjal.Day49;
+
+public class NameArray {
+  public static void main(String[] args) {
+    char[] name = {'P', 'R', 'A', 'N', 'J', 'A', 'L'};
+
+    System.out.println("Letters in my name:");
+    for (int i = 0; i < name.length; i++) {
+        System.out.print(name[i] + " ");
+    }
+}
+
+}
